e1f9176cc982c055723482be0d5e5fb9cfae4a46
VitorAlvesCarvalho <vitor.alves2402@gmail.com>
2023-01-24 23:15:35 -0300

    chore: adicionando fonte ao projeto

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,8 @@ export default {
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Roboto+Condensed");
+
 * {
   padding: 0;
   margin: 0;
@@ -24,6 +26,7 @@ export default {
 }
 
 .app {
+  font-family: "Roboto", sans-serif;
   background-color: #242424;
   width: 100%;
   min-height: 100vh;
